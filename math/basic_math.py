e88327af19a7b04fc4d00055b3c597f520dbaa00
Naniyaking <naniyaking@gmail.com>
2021-03-10 17:02:46 +0800

    Create basic_math.py

---
+++ math/basic_math.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+def get_num(num):
+    if num == 1:
+       print(num,"不是素数")
+    else:
+    
+        for i in range(2,num):
+            if  num % i == 0:#取余等于10,说明有能被整除的数
+                # print(i)
+                print(num,"不是素数")
+                j = int(num/i)   
+                print(j,"*",i,"=",num)
+                break 
+        else:
+            # print(num)
+            print(num,"是素数")
+
+    
+        
+        
+
+        
+    # print(num,"是素数")
+
+if __name__ == '__main__':
+    for num in range(1, 100):
+        get_num(num)
+    # print(95 % 2)
